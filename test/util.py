5649e4d5ae3636e48d4b511a5011766d824b505e
louchenyao <louchenyao@gmail.com>
2020-08-05 02:40:11 +0000

    prebuilt dbgen binary

--- test/util.py
+++ test/util.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import argparse
 import os
 
@@ -17,17 +19,17 @@ def gen_data(dataset, scale_factor):
     path = './' + dataset + '/dbgen/'
     with cd(path):
         os.system('rm -rf *.tbl')
-        os.system('./dbgen -s %d -T a' % scale_factor)
+        os.system('./dbgen -s %d' % scale_factor)
         os.system('mkdir -p ../data/s%d' % scale_factor)
         os.system('mv *.tbl ../data/s%d/' % scale_factor)
 
 def transform(dataset, scale_factor):
     path = './' + dataset + '/loader/'
-    ip = '../data/s%d/' % scale_factor
-    op = '../data/s%d_columnar/' % scale_factor
+    ip = '../data/s%d' % scale_factor
+    op = '../data/s%d_columnar' % scale_factor
     with cd(path):
         os.system('mkdir -p %s' % op)
-        os.system('python convert.py ../data/s%d/' % scale_factor)
+        os.system('python3 convert.py ../data/s%d/' % scale_factor)
         os.system('./loader --lineorder %s/lineorder.tbl --ddate %s/date.tbl --customer %s/customer.tbl.p --supplier %s/supplier.tbl.p --part %s/part.tbl.p --datadir %s' % (ip, ip, ip, ip, ip, op))
 
 if __name__ == "__main__":
